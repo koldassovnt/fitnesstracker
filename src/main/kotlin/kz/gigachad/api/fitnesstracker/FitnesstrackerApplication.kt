2b96bb718909b324cae70ac81384669341d7dd70
koldassovnt <koldassovnt@gmail.com>
2023-04-25 12:48:26 +0600

    init commit

---
+++ src/main/kotlin/kz/gigachad/api/fitnesstracker/FitnesstrackerApplication.kt
@@ -0,0 +1,11 @@
+package kz.gigachad.api.fitnesstracker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FitnesstrackerApplication
+
+fun main(args: Array<String>) {
+	runApplication<FitnesstrackerApplication>(*args)
+}
---
+++ src/test/kotlin/kz/gigachad/api/fitnesstracker/FitnesstrackerApplicationTests.kt
@@ -0,0 +1,13 @@
+package kz.gigachad.api.fitnesstracker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FitnesstrackerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
